29a24d0176a59e82cf625f96aaca724078a58681
imakespaghetticode <44702379+imakespaghetticode@users.noreply.github.com>
2018-11-03 13:00:18 +0100

    website command + organization of code

--- bot.py
+++ bot.py
@@ -8,13 +8,30 @@ with open("bot.json") as file:
 
 bot = commands.Bot(command_prefix="rg!")
 
+@bot.event
+async def on_ready():
+	print("Bot ready")
+
+
+# Ping command
+
 @bot.command()
 async def ping(ctx):
     await ctx.send(str(int(bot.latency * 1000))+ "ms")
 
+# Invite command
+
 @bot.command()
 async def invite(ctx):
 	await ctx.send("https://bit.ly/2PDuIkc")
+
+# Website command
+
+@bot.command()
+async def website(ctx):
+	await ctx.send("http://roboticgangsta.surge.sh")
+
+# Github command
 
 @bot.command()
 async def github(ctx):
